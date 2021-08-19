e96ce412aee6cfe99b23f01fddc47699a2fc37cd
keshan-spec <keshanth.programmer@gmail.com>
2021-08-19 19:36:02 +0530

    animations for nav bar

--- main.js
+++ main.js
@@ -5,4 +5,11 @@ const navLinks = document.getElementById("links")
 btn.addEventListener("click", (e) => {
     e.preventDefault()
     navLinks.classList.toggle("active")
+
+    if (navLinks.classList.contains("active")) {
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+    }
+    else {
+        btn.innerHTML = '<i class="fas fa-bars"></i>'
+    }
 })
